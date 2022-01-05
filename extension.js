8dadd0e63f189db2cadb4a771b9d894d7859b9f2
Giiyms <giiyms@fishbowl.dev>
2022-01-05 17:04:33 +0300

    first commit

--- extension.js
+++ extension.js
@@ -1,46 +1,23 @@
-const vscode = require('vscode')
+const vscode = require("vscode");
 
-function colonize (option) {
-  var editor = vscode.window.activeTextEditor
-  if (!editor) return
+function debugeval() {
+  var editor = vscode.window.activeTextEditor;
+  if (!editor) return;
 
-  vscode.commands.executeCommand('acceptSelectedSuggestion').then(() => {
-    var lineIndex = editor.selection.active.line
-    var lineObject = editor.document.lineAt(lineIndex)
-    var lineLength = lineObject.text.length
+  if (!vscode.env.debugInfo.debug) return;
 
-    if (lineObject.text.charAt(lineLength - 1) !== ';' && !lineObject.isEmptyOrWhitespace) {
-      var insertionSuccess = editor.edit((editBuilder) => {
-        editBuilder.insert(new vscode.Position(lineIndex, lineLength), ';')
-      })
-
-      if (!insertionSuccess) return
-    }
-
-    if (option === 'hold') return
-
-    option === 'endline'
-      ? vscode.commands.executeCommand('cursorEnd')
-      : vscode.commands.executeCommand('editor.action.insertLineAfter')
-  })
+  vscode.commands.executeCommand("acceptSelectedSuggestion").then(() => {
+    vscode.commands.executeCommand("expandLineSelection");
+    vscode.commands.executeCommand("editor.debug.action.selectionToRepl");
+  });
 }
 
-function activate (context) {
-  var endLineDisposable = vscode.commands.registerCommand('colonize.endline', () => {
-    colonize('endline')
-  })
+function activate(context) {
+  var debugEval = vscode.commands.registerCommand("debugeval", () => {
+    debugeval();
+  });
 
-  var holdDisposable = vscode.commands.registerCommand('colonize.hold', () => {
-    colonize('hold')
-  })
-
-  var newLineDisposable = vscode.commands.registerCommand('colonize.newline', () => {
-    colonize('newline')
-  })
-
-  context.subscriptions.push(endLineDisposable)
-  context.subscriptions.push(newLineDisposable)
-  context.subscriptions.push(holdDisposable)
+  context.subscriptions.push(debugEval);
 }
 
-exports.activate = activate
+exports.activate = activate;
